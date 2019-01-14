4e93b919d1d61d9e7f88209479c702ff7dc85ce9
Marga Martinez <margamarben@gmail.com>
2019-01-14 21:07:04 +0100

    Add clock and clockKitKat projects

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+/* clock */ 
+const secHand = document.querySelector(".secondHand");
+const minsHand = document.querySelector(".minHand");
+const hoursHand = document.querySelector(".hourHand");
+
+function setDate() {
+
+    const now = new Date();
+
+    
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90; /* 360 es por los grados del circulo y 90 es por el transform: rotate(90deg); de la clase hand */
+    secHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hoursHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000); /* cada segundo se actualice*/ 
+
+setDate();
+
 // $(document).ready(() => {
 
 //     $(".latte").on("click", () => { 
